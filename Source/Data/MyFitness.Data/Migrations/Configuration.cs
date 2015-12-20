758e596a00f1ea010bc72144a0d6170b002d861c
elena-andonova <ellie.andonova@gmail.com>
2015-12-20 20:56:24 +0200

    Fixed styles for displaying fitness programs..

--- Source/Data/MyFitness.Data/Migrations/Configuration.cs
+++ Source/Data/MyFitness.Data/Migrations/Configuration.cs
@@ -13,48 +13,48 @@ namespace MyFitness.Data.Migrations
 
         protected override void Seed(MyFitnessDbContext context)
         {
-      //    var category = new Category
-      //    {
-      //        Name = "WeightLoss"
-      //    };
-      // 
-      //    context.Categories.Add(category);
-      // 
-      //    context.SaveChanges();
-      // 
-      //    var exercise = new Exercise
-      //    {
-      //        Category = category,
-      //        Description = "This is deep description",
-      //        Name = "Pesho trenira"
-      //    };
-      // 
-      //    var exercise2 = new Exercise
-      //    {
-      //        Category = category,
-      //        Description = "This is no description",
-      //        Name = "Pesho pee"
-      //    };
-      // 
-      //    context.Exercises.Add(exercise);
-      //    context.Exercises.Add(exercise2);
-      // 
-      //    context.SaveChanges();
-      // 
-      //    var fitnessProgram = new FitnessProgram
-      //    {
-      //        Name = "Good program",
-      //        Description = "Really  good program",
-      //        SuitableFor = Suitable.Novice,
-      //        Category = category
-      //    };
-      // 
-      //    fitnessProgram.Exercises.Add(exercise);
-      //    fitnessProgram.Exercises.Add(exercise2);
-      // 
-      //    context.FitnessPrograms.Add(fitnessProgram);
-      // 
-      //    context.SaveChanges();
+            var category = new Category
+            {
+                Name = "WeightLoss"
+            };
+
+            context.Categories.Add(category);
+
+            context.SaveChanges();
+
+            var exercise = new Exercise
+            {
+                Category = category,
+                Description = "This is deep description",
+                Name = "Pesho trenira"
+            };
+
+            var exercise2 = new Exercise
+            {
+                Category = category,
+                Description = "This is no description",
+                Name = "Pesho pee"
+            };
+
+            context.Exercises.Add(exercise);
+            context.Exercises.Add(exercise2);
+
+            context.SaveChanges();
+
+            var fitnessProgram = new FitnessProgram
+            {
+                Name = "Good program",
+                Description = "Really  good program",
+                SuitableFor = Suitable.Novice,
+                Category = category
+            };
+
+            fitnessProgram.Exercises.Add(exercise);
+            fitnessProgram.Exercises.Add(exercise2);
+
+            context.FitnessPrograms.Add(fitnessProgram);
+
+            context.SaveChanges();
         }
     }
 }
--- Source/Universal/MyFitness.Universal/ViewModels/FitnessProgramViewModel.cs
+++ Source/Universal/MyFitness.Universal/ViewModels/FitnessProgramViewModel.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyFitness.Universal.ViewModels
+﻿namespace MyFitness.Universal.ViewModels
 {
+    using System.Collections.Generic;
+
     public class FitnessProgramViewModel
     {
         public int Id { get; set; }
