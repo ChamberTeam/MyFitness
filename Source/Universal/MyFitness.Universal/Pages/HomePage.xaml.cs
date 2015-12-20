2a4ea52f538c7f8209cec7f0e686f2bffbd587f3
elena-andonova <ellie.andonova@gmail.com>
2015-12-20 18:48:26 +0200

    Login and Register positions..

--- Source/Universal/MyFitness.Universal/Pages/HomePage.xaml.cs
+++ Source/Universal/MyFitness.Universal/Pages/HomePage.xaml.cs
@@ -4,20 +4,48 @@
     using MyFitness.Universal.ViewModels;
     using Windows.UI.Xaml;
     using Windows.UI.Xaml.Controls;
-
+    using Windows.UI.Xaml.Input;
     public sealed partial class HomePage : Page
     {
         public HomePage()
         {
             this.InitializeComponent();
-            this.DataContext = new HomePageViewModel();
+
+            this.HomePageViewModel = new HomePageViewModel();
+            //this.IsUserLogged();
         }
 
-        public void Test()
+        public HomePageViewModel HomePageViewModel
         {
-            var user = new UsersService();
-            var fitness = new FitnessProgramsService();
-            user.LoginUser("PROBA", "123456");
+            get
+            {
+                return this.DataContext as HomePageViewModel;
+            }
+            set
+            {
+                this.DataContext = value;
+            }
+        }
+
+        //private async void IsUserLogged()
+        //{
+        //    var user = await this.HomePageViewModel.UserViewModel.GetUserAsync();
+        //    if (user != null)
+        //    {
+        //        this.loginBtn.Visibility = Visibility.Collapsed;
+        //        this.registerBtn.Visibility = Visibility.Collapsed;
+        //        //this.favouriteBtn.Visibility = Visibility.Visible;
+        //    }
+        //}
+
+        private void OnRegisterBtnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Register), null);
+        }
+
+        private void OnloginBtnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Login), null);
         }
     }
 }
